a79a45443c0e1015cf71fb9830b75b3092242751
bhardwajudhay <96589808+bhardwajudhay@users.noreply.github.com>
2022-11-12 19:08:05 +0530

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,12 @@
+public class Maths {
+    public static void main(String args[]) {
+        System.out.println("Program to find number divisible by 23 between 10 and 150");
+        int n;
+//        int i;
+        for (int i = 10; i<=150;i++){
+            if (i % 23 == 0) {
+                System.out.println(i);
+//            }  else{System.out.println("not divisible");
+        }
+    }
+}}
